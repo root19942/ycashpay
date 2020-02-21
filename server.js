633d252709ffa9d40a6a8e859be1ee036940cc08
ket-up <ycashpay@gmail.com>
2020-02-21 15:20:03 +0100

    serve

--- server.js
+++ server.js
@@ -33,8 +33,9 @@ io.on('connection', (socket) => {
     });
 
 
-  console.log('Client connected');
+
   socket.on('disconnect', () => console.log('Client disconnected'));
+  
 });
 
 setInterval(() => io.emit('time', new Date().toTimeString()), 1000);
