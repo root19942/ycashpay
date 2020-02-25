d939ece31d9add4167c297d34169ebf5a4c27281
root19942 <42292743+root19942@users.noreply.github.com>
2020-02-25 19:50:57 +0100

    Update server.js

--- server.js
+++ server.js
@@ -35,7 +35,9 @@ io.on('connection', (socket) => {
 
     });
 
-
+    socket.on('onPaiement', (paiement) => {
+	io.emit('paiment', new Date().toTimeString())
+    )};
 
   socket.on('disconnect', () => console.log('Client disconnected'));
   
